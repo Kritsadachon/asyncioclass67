958096f260826bdbba95167e94974f19996a06e0
Kritsadachon <87796673+Kritsadachon@users.noreply.github.com>
2024-06-21 11:57:11 +0700

    complete2

--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,15 @@
 # running a function with arguments in another thread
+from time import sleep, ctime 
+from threading import Thread
+
+def task(sleep_time, message):
+    #block for a moment
+    sleep(sleep_time)
+    #then display msg
+    print(f'{ctime()} {message}')
+
+thread = Thread(target=task, args=(1.5, 'Hello Kub'))
+#run thread
+thread.start()
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
