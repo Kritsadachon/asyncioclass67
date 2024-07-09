3e0260801d21ae8f0dd1bffdda1912f01c8ff95c
Kritsadachon <87796673+Kritsadachon@users.noreply.github.com>
2024-07-09 20:25:17 +0700

    .

--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -1,3 +1,9 @@
 # Asynchronous breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee(): #1
+    print("coffee: prepare")
+    sleep(1)
+    print("coffee waiting")
+    await asyncio.sleep(5)
