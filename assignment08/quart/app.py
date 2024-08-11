0057187e739e479111ea243f981fbd65d87fbe30
Kritsadachon <87796673+Kritsadachon@users.noreply.github.com>
2024-08-12 01:57:08 +0700

    add assignment8
    add assignment8

--- assignment08/quart/app.py
+++ assignment08/quart/app.py
@@ -1,30 +1,42 @@
-from quart import Quart
-from quart import render_template
-
-from pypokemon.pokemon import Pokemon
+from quart import Quart, render_template
 import asyncio
 import httpx
 import time
 import random
+from pypokemon.pokemon import Pokemon
 
 app = Quart(__name__)
 
 async def get_pokemon(client, url):
-    pass
-
-
-
+    print(f"{time.ctime()} - get {url}")
+    resp = await client.get(url)
+    pokemon = resp.json()
+    return pokemon
 
 async def get_pokemons():
+    rand_list = [random.randint(1, 151) for _ in range(5)]
+    pokemon_data = []
+
     async with httpx.AsyncClient() as client:
-        pass
+        tasks = []
+        for number in rand_list:
+            url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+            tasks.append(asyncio.create_task(get_pokemon(client, url)))
 
+        pokemons_json = await asyncio.gather(*tasks)
+        for pokemon_json in pokemons_json:
+            pokemon_object = Pokemon(pokemon_json)
+            pokemon_data.append(pokemon_object)
 
-
+    return pokemon_data
 
 @app.route('/')
 async def index():
-    pass
+    start_time = time.perf_counter()
+    pokemons = await get_pokemons() 
+    end_time = time.perf_counter()
+    print(f"{time.ctime()} - Get {len(pokemons)} pokemons. Time taken: {end_time-start_time} seconds")
+    return await render_template('index.html', pokemons=pokemons, end_time=end_time, start_time=start_time)
 
 if __name__ == '__main__':
     app.run(debug=True, port=50002)
